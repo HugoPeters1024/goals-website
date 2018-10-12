6f2d8e02c4cf6ab81ee5fef77f8da69d43daa2eb
HugoPeters1024 <hpeters1024@gmail.com>
2018-10-12 17:49:55 +0200

    ticket for the ns

--- routes.js
+++ routes.js
@@ -16,4 +16,10 @@ module.exports = (app) => {
 
 
 	app.get('/coding', (req, res) => res.render('pages/coding'));
+	app.get('/ticket', (req, res) => res.render('pages/ticket', { ticket: {
+		"Destination" : req.query.destination,
+		"Origin" : req.query.origin,
+		"Type" : req.query.type,
+		"Price" : req.query.price
+	} }));
 }
